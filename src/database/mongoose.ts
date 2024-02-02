ec0789620dbb847305e59b1174a2b09fbab922e5
Deepakkk21 <dk135781@gmail.com>
2024-02-02 15:08:37 +0530

    done

--- src/database/mongoose.ts
+++ src/database/mongoose.ts
@@ -1,11 +1,11 @@
 import mongoose from 'mongoose';
 
-const url = 'mongodb://127.0.0.1:27017/wasserstoff';
-// const url ='mongodb+srv://Deepakk:Deepak2110@cluster0.tt4vu2r.mongodb.net/?retryWrites=true&w=majority';
+// const url = 'mongodb://127.0.0.1:27017/wasserstoff';
+const url ='mongodb+srv://Deepakk:Deepak2110@cluster0.tt4vu2r.mongodb.net/?retryWrites=true&w=majority';
 mongoose.connect(url);
 
 const connection = mongoose.connection;
-
+ 
 connection.once('open', () => {
   console.log('MongoDB database connection established successfully');
 });
