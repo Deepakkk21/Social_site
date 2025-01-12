8c6d49657dca38108cb593e51e4e7cfd07a79b8c
Deepak <113232000+Deepakkk21@users.noreply.github.com>
2025-01-12 10:28:17 +0530

    Update mongoose.ts

--- src/database/mongoose.ts
+++ src/database/mongoose.ts
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose';
 
 // const url = 'mongodb://127.0.0.1:27017/wasserstoff';
-const url ='mongodb+srv://Deepakk:Deepak2110@cluster0.tt4vu2r.mongodb.net/?retryWrites=true&w=majority';
+// const url ='mongodb+srv://Deepakk:Deepak2110@cluster0.tt4vu2r.mongodb.net/?retryWrites=true&w=majority';
+const url ='mongodb+srv://Deepak:Deep@k2110@social.fyqcqkz.mongodb.net/?retryWrites=true&w=majority&appName=social';
 mongoose.connect(url);
 
 const connection = mongoose.connection;
